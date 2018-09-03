6925729545960ebc18fd3845d18a2f367fccda77
tiffany <tiffany831101@gmail.com>
2018-09-03 22:44:19 +0800

    finished hw1

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,15 @@
 function printStars(n) {
-  
+var str = ""; 
+var newnum = (n+1)*n/2; 
+for(var i=1;i<=newnum;i++){    
+if(i!==newnum){
+	str+= "*"    
+	str+="\n";   
 }
+else{
+	str+="*"
+} 
+} 
+console.log(str);
+}  
+
