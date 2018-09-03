5d32bb0ec8de8aa51f7fc043a5ae66cedf8612b6
tiffany <tiffany831101@gmail.com>
2018-09-03 23:36:51 +0800

    finished hw4

--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -1,3 +1,9 @@
 function printFactor(n) {
-
+    var array = [];
+    for (var i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            array.push(i);
+        }
+    }
+    console.log(array);
 }
