5de3886c2e85b398c1ef667d48987ff25cc19d2a
tiffany <tiffany831101@gmail.com>
2018-09-05 00:27:00 +0800

    week2_hw1-6

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,11 @@
 function stars(n) {
-
+	var array = [];    
+	var str = "";    
+	for(var i=0;i<n;i++){       
+		str+="*";       
+		array.push(str);    
+	}    
+	console.log(array);
 }
 
 module.exports = stars;
