307ca7c22ca53bcab6d8dabbdda00622739d23b3
tiffany <tiffany831101@gmail.com>
2018-09-09 00:43:22 +0800

    correctmistakes

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,20 +1,24 @@
-function add(a, b) {
+function add(a,b){    
 	var array1 = a.split("");    
 	var array2 = b.split("");    
-	var array3 = []; 
-	for(var i=array1.length-1;i>=0;i--){    
-		var num = Number(array1[i])+Number(array2[i]);       
-		var lastnum = Number(array1[i+1])+Number(array2[i+1]);       
-		var total = num + Math.floor(lastnum/10);    
-		if(i!==array1.length-1&&i!==0){        
-			array3.push(total%10);     
-		}else if(i==0){        
-			array3.push(total);      
-		}else{       
-			array3.push(num%10);      
+	var array3 = [];    
+	if(array1.length==1){       
+		console.log(Number(a)+Number(b)+"");    
+	}else{
+		for(var i=array1.length-1;i>=0;i--){    
+			var num = Number(array1[i])+Number(array2[i]);       
+			var lastnum = Number(array1[i+1])+Number(array2[i+1]);       
+			var total = num + Math.floor(lastnum/10);    
+			if(i!==array1.length-1&&i!==0){        
+				array3.push(total%10);     
+			}else if(i==0){        
+				array3.push(total);      
+			}else{       
+				array3.push(num%10);      
+			}    
 		}    
-	}    
-	return array3.reverse().join("");   
+		console.log(array3.reverse().join(""));  
+	}      
 }
 
 module.exports = add;
